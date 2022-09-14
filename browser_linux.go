7f36046fa77dcdd36482e267a18f49215a34bfd8
Armin <arminaaki@users.noreply.github.com>
2022-09-13 21:53:52 -0400

    handle wsl detection after providers list is tried

--- browser_linux.go
+++ browser_linux.go
@@ -11,6 +11,15 @@ import (
 func openBrowser(url string) error {
 	providers := []string{"xdg-open", "x-www-browser", "www-browser"}
 
+	// There are multiple possible providers to open a browser on linux
+	// One of them is xdg-open, another is x-www-browser, then there's www-browser, etc.
+	// Look for one that exists and run it
+	for _, provider := range providers {
+		if _, err := exec.LookPath(provider); err == nil {
+			return runCmd(provider, url)
+		}
+	}
+
 	wsl, err := isWSL()
 	if err != nil {
 		return errors.New("error determining Windows Subsystem for Linux")
@@ -22,15 +31,6 @@ func openBrowser(url string) error {
 		url := strings.ReplaceAll(url, "&", "^&")
 		args = append(args, url)
 		return exec.Command(cmd, args...).Start()
-	}
-
-	// There are multiple possible providers to open a browser on linux
-	// One of them is xdg-open, another is x-www-browser, then there's www-browser, etc.
-	// Look for one that exists and run it
-	for _, provider := range providers {
-		if _, err := exec.LookPath(provider); err == nil {
-			return runCmd(provider, url)
-		}
 	}
 
 	return &exec.Error{Name: strings.Join(providers, ","), Err: exec.ErrNotFound}
